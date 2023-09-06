792ba131d7348e18b2878e9e8b4c2fa05b7f95ed
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2023-09-06 20:13:44 -0300

    commit

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 import { Router } from "./router.js";
 
 const router = new Router();
-router.addRoute("/home.html", "/pages/home.html");
-router.addRoute("/universe.html", "/pages/universe.html");
-router.addRoute("/exploration.html", "/pages/exploration.html");
+router.addRoute("/home.html", "./pages/home.html");
+router.addRoute("/universe.html", "./pages/universe.html");
+router.addRoute("/exploration.html", "./pages/exploration.html");
 
 
-router.addImage("/home.html", "/assets/mountains-universe.png");
-router.addImage("/universe.html", "/assets/mountains-universe02.png");
-router.addImage("/exploration.html", "/assets/mountains-universe-3.png");
+router.addImage("/home.html", "./assets/mountains-universe.png");
+router.addImage("/universe.html", "./assets/mountains-universe02.png");
+router.addImage("/exploration.html", "./assets/mountains-universe-3.png");
 
 window.route = () => router.handle();
 

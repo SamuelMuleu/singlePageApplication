12420e13ef2ab8205f8374fd614375647416ac59
Samuel Pereira Muleu <samuelpereiramuleu@gmail.com>
2023-09-05 22:18:43 -0300

    first commit

---
+++ js/router.js
@@ -0,0 +1,35 @@
+export  class Router {
+  routes = {};
+
+  addRoute(routeName, page) {
+    this.routes[routeName] = page;
+  }
+  imagens = {};
+  addImage(imageName, link) {
+    this.imagens[imageName] = link;
+  }
+
+  route(event) {
+    event = event || window.event;
+    event.preventDefault(); //serve para o evento nao fazer o padrao
+
+    window.history.pushState({}, "", event.target.href);
+
+    this.handle();
+  }
+  handle() {
+    const { pathname } = window.location;
+
+    const route = this.routes[pathname] || this.routes["/404.html"];
+    const backgroundImageUrl = this.imagens[pathname] || this.imagens["/home.html"];
+
+    fetch(route)
+      .then((data) => data.text())
+      .then((html) => {
+        document.querySelector("#app").innerHTML = html;
+        document.body.style.backgroundImage = `url(${backgroundImageUrl})`;
+      });
+  }
+}
+
+
